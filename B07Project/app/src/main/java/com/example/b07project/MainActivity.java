8029d0adc8a4e3d2e674d66135acbf02c33d29a4
samnaniaahil <aahil.samnani@yahoo.com>
2022-11-24 19:49:36 -0500

    Implemented activity and layout for student registration (user story 1)

--- B07Project/app/src/main/java/com/example/b07project/MainActivity.java
+++ B07Project/app/src/main/java/com/example/b07project/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.b07project.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Button button = (Button)findViewById(R.id.register_page_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                /*myIntent.putExtra("key", value); //Optional parameters*/
+                startActivity(registerIntent);
+            }
+        });
+
     }
 }
