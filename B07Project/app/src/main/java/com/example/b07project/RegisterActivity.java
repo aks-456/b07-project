e3c181e52677fbd931b43315a776d58c76c71de1
samnaniaahil <aahil.samnani@yahoo.com>
2022-11-27 16:43:18 -0500

    Change register page to display login errors and match login theme
    Add back button to login page, adjust font sizes, and alert user if they violate email and password requirements

--- B07Project/app/src/main/java/com/example/b07project/RegisterActivity.java
+++ B07Project/app/src/main/java/com/example/b07project/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,30 +19,53 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
-//    private AppBarConfiguration appBarConfiguration;
-//    private ActivityRegisterBinding binding;
+    //private AppBarConfiguration appBarConfiguration;
+    //private ActivityRegisterBinding binding;
 
     private static final String TAG = "EmailPassword";
     private FirebaseAuth mAuth;
-    private String email;
-    private String password;
+    EditText emailBox;
+    EditText passwordBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        EditText emailBox = findViewById(R.id.editTextTextEmailAddress);
-        EditText passwordBox = findViewById(R.id.editTextTextPassword);
+        emailBox = findViewById(R.id.editTextEmailAddress);
+        passwordBox = findViewById(R.id.editTextPassword);
         mAuth = FirebaseAuth.getInstance();
 
         Button button = findViewById(R.id.register_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 email = emailBox.getText().toString();
-                 password = passwordBox.getText().toString();
+                String email = emailBox.getText().toString();
+                String password = passwordBox.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    emailBox.setError("Email Cannot Be Empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    passwordBox.setError("Password Cannot Be Empty");
+                    return;
+                }
+                if (password.length() < 6) {
+                    passwordBox.setError("Password Must Be At least 6 Characters Long");
+                    return;
+                }
+
                 createAccount(email, password);
+            }
+        });
+
+        Button backButton = findViewById(R.id.register_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(RegisterActivity.this, login_activity.class);
+                startActivity(loginIntent);
             }
         });
     }
@@ -51,7 +75,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null) {
             reload();
         }
     }
@@ -80,7 +104,7 @@ public class RegisterActivity extends AppCompatActivity {
     private void reload() {}
 
     private void updateUI(FirebaseUser user) {
-        if(user != null) {
+        if (user != null) {
             Intent registerSuccessIntent = new Intent(RegisterActivity.this, login_activity.class);
             startActivity(registerSuccessIntent);
         }
