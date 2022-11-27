b0c8026279e9b283b95ed7eeeabd2e489770038a
Dhruvin Patel <104400084+pdhruvin25@users.noreply.github.com>
2022-11-26 19:47:45 -0500

    Dhruvin and Ahil merge (Login page and registration page)
    a merge of the login and registration page

---
+++ B07Project/app/src/main/java/com/example/b07project/RegisterActivity.java
@@ -0,0 +1,88 @@
+package com.example.b07project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+//    private AppBarConfiguration appBarConfiguration;
+//    private ActivityRegisterBinding binding;
+
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EditText emailBox = findViewById(R.id.editTextTextEmailAddress);
+        EditText passwordBox = findViewById(R.id.editTextTextPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+        Button button = findViewById(R.id.register_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 email = emailBox.getText().toString();
+                 password = passwordBox.getText().toString();
+                createAccount(email, password);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void reload() {}
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+            Intent registerSuccessIntent = new Intent(RegisterActivity.this, admin_or_student.class);
+            startActivity(registerSuccessIntent);
+        }
+    }
+}
