a3326563abeca2aaa931a1b984b8dafa4295b2aa
aks-456 <akshath.sohal@gmail.com>
2022-11-28 21:43:23 -0500

    Added dropdown for add course for admin
    Added dropdown for admin to choose prerequisite courses when adding a new course

--- B07Project/app/src/main/java/com/example/b07project/AddCourseAdmin.java
+++ B07Project/app/src/main/java/com/example/b07project/AddCourseAdmin.java
@@ -1,21 +1,32 @@
 package com.example.b07project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class AddCourseAdmin extends AppCompatActivity {
 
     EditText etCourseName, etCourseCode, etCourseOfferings, etPrerequisites;
-    Button btnAddCourse;
+    Button btnAddCourse, btnAddPre, btnAddOff;
+    Spinner spPre, spOff;
 
     private DatabaseReference mDatabase;
     @Override
@@ -25,25 +36,101 @@ public class AddCourseAdmin extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        etCourseName = findViewById(R.id.etCourseName);
-        etCourseCode = findViewById(R.id.etCourseCode);
-        etCourseOfferings = findViewById(R.id.etCourseOfferings);
-        etPrerequisites = findViewById(R.id.etPrerequisities);
-        btnAddCourse = findViewById(R.id.btnAddCourse);
+        etCourseName = (EditText)findViewById(R.id.etCourseName);
+        etCourseCode = (EditText)findViewById(R.id.etCourseCode);
+        etCourseOfferings = (EditText)findViewById(R.id.etCourseOfferings);
+        etPrerequisites = (EditText)findViewById(R.id.etPrerequisities);
+        btnAddCourse = (Button)findViewById(R.id.btnAddCourse);
+        btnAddPre = (Button)findViewById(R.id.btnAddPre);
+        btnAddOff = (Button)findViewById(R.id.btnAddOff);
+        spOff = (Spinner)findViewById(R.id.spOff);
+        spPre = (Spinner)findViewById(R.id.spPre);
 
+        //Get current signed in user
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = "";
+        if (user != null) {
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            uid = user.getUid();
+        }
+
+        //Populate array for Prerequisites Dropdown
+        ArrayList<String> preItems=new ArrayList<String>();
+        preItems.add("Choose Prerequisite(s)");
+
+        //Get list of courses from firebase -- from admin id
+        mDatabase.child("admin_courses").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(AddCourseAdmin.this, "There are no courses added to select prerequisites, please enter them manually", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    for(DataSnapshot ds : task.getResult().getChildren()) {
+                        String key = ds.getKey();
+                        preItems.add(key);
+                    }
+
+                }
+            }
+        });
+
+        // use default spinner item to show options in spinner
+        ArrayAdapter<String> preAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, preItems);
+        spPre.setAdapter(preAdapter);
+
+        btnAddPre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String current = etPrerequisites.getText().toString().trim();
+                String newer = "";
+                if(!(spPre.getSelectedItem().toString().equals("Choose Prerequisite(s)"))) {
+                    if(current.equals("")) {
+                        newer = current+ spPre.getSelectedItem().toString();
+                    } else {
+                        newer = current + "," + spPre.getSelectedItem().toString();
+                    }
+                    etPrerequisites.setText(newer);
+
+                }
+            }
+        });
+
+        //Course offering sessions
+        ArrayList<String> offItems=new ArrayList<String>();
+        offItems.add("Choose Offering(s)");
+        offItems.add("Winter");
+        offItems.add("Summer");
+        offItems.add("Fall");
+
+        // use default spinner item to show options in spinner
+        ArrayAdapter<String> offAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, offItems);
+        spOff.setAdapter(offAdapter);
 
         btnAddCourse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String strCourseName = etCourseName.getText().toString().trim();
-                String strCourseCode = etCourseOfferings.getText().toString().trim();
+                String strCourseCode = etCourseCode.getText().toString().trim();
                 String strPrerequisites = etPrerequisites.getText().toString().trim();
                 String strOfferings = etCourseOfferings.getText().toString().trim();
 
-//                String [] arrPrerequisites = strPrerequisites.split(",");
-//                String [] arrOfferings = strOfferings.split(",");
+                //Get current signed in user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = "";
+                if (user != null) {
 
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getIdToken() instead.
+                    uid = user.getUid();
+                }
 
+                //Change to uid
+                writeNewUser(strCourseName, strCourseCode, strPrerequisites, strOfferings);
 
             }
         });
@@ -52,10 +139,10 @@ public class AddCourseAdmin extends AppCompatActivity {
     }
 
 
-    public void writeNewUser(String courseId, String name, String code, String prerequisites, String offerings) {
+    public void writeNewUser(String name, String code, String prerequisites, String offerings) {
+        //Check if the course code already exists
         Course course = new Course(name, code, prerequisites, offerings);
-        mDatabase.child("courses").child(courseId).setValue(course);
-
+        mDatabase.child("admin_courses").child(code).setValue(course);
     }
 
 
