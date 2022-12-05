ddaae9ed57b702049ab1ab2ee847e36aaf343b25
KevinT928 <96837880+KevinT928@users.noreply.github.com>
2022-12-04 23:13:44 -0500

    Update DisplayTakenCourseActivity.java

--- B07Project/app/src/main/java/com/example/b07project/DisplayTakenCourseActivity.java
+++ B07Project/app/src/main/java/com/example/b07project/DisplayTakenCourseActivity.java
@@ -12,6 +12,8 @@ import android.widget.Button;
 import android.widget.ListView;
 
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
