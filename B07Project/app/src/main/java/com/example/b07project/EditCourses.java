bbda8f97af034f59582f3a767984a28560e5d378
Dhruvin Patel <104400084+pdhruvin25@users.noreply.github.com>
2022-12-05 18:05:04 -0500

    Semi-Final Commit

--- B07Project/app/src/main/java/com/example/b07project/EditCourses.java
+++ B07Project/app/src/main/java/com/example/b07project/EditCourses.java
@@ -1,8 +1,6 @@
 package com.example.b07project;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -14,7 +12,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,10 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.ArrayList;
-
-
 public class EditCourses extends AppCompatActivity {
     ListView listView;
     ArrayList<String> arr = new ArrayList<>();
@@ -42,7 +36,6 @@ public class EditCourses extends AppCompatActivity {
         add_course = findViewById(R.id.button3);
         ArrayAdapter<String> arr2 = new ArrayAdapter<>(this, R.layout.textviewlayout, arr);
         database = FirebaseDatabase.getInstance().getReference();
-
         database.child("admin_courses").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -52,12 +45,11 @@ public class EditCourses extends AppCompatActivity {
                 else {
                     for(DataSnapshot ds : task.getResult().getChildren()) {
 
-                        String key = ds.getKey() + "\n"+ ds.child("name").getValue() + "\nOFFERED: " + ds.child("offerings").getValue() + "\nPREREQUISITES: " + ds.child("prerequisites").getValue();
+                        String key =ds.getKey() + "\n"+ ds.child("name").getValue() + "\nOFFERED: " + ds.child("offerings").getValue() + "\nPREREQUISITES: " + ds.child("prerequisites").getValue();
                         arr.add(key);
                     }
                     listView.setAdapter(arr2);
                 }
-
             }
         });
         add_course.setOnClickListener(new View.OnClickListener() {
@@ -73,8 +65,6 @@ public class EditCourses extends AppCompatActivity {
 //                startActivity(new Intent(EditCourses.this, EditCourses.class));
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(EditCourses.this, login_activity.class));
-
-
             }
         });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -94,22 +84,9 @@ public class EditCourses extends AppCompatActivity {
                         // Call edit func
                         Intent intent = new Intent(EditCourses.this, EditCourseAdmin.class);
                         intent.putExtra("course_code", arr.get(i));
-                        startActivityForResult(intent, i);
+                        startActivityForResult(intent, 1);
                         arr2.notifyDataSetChanged();
-
-
-
                     }
-                    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-                        super.onActivityResult(requestCode, resultCode, data);
-                        if (requestCode == 1) {
-                            if(resultCode == RESULT_OK) {
-                                String strEditText = data.getStringExtra("key");
-
-                                // update your listview in here with new data
-                            }
-                        }
-
                 });
                 builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
@@ -119,13 +96,9 @@ public class EditCourses extends AppCompatActivity {
                 });
                 AlertDialog dialog = builder.create();
                 dialog.show();
-
-
-
             }
             private void deleteCourse(String courseCode) {
                 Log.e("TAG", courseCode);
-
                 database.child("admin_courses").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -135,27 +108,28 @@ public class EditCourses extends AppCompatActivity {
                         else {
                             for (DataSnapshot ds: task.getResult().getChildren()) {
                                 String[] prereqs = task.getResult().child(ds.getKey()).child("prerequisites").getValue().toString().split(",");                                    int i = 0;
-                                    boolean courseInList = false;
-                                    while (i < prereqs.length && !courseInList) {
-                                        if (prereqs[i].equals(courseCode)) {
-                                            courseInList = true;
-                                            String newPrereqs = "";
-                                            for (int j = 0; j < prereqs.length; j++) {
-                                                if (j != i) {
-                                                    newPrereqs += prereqs[j] + ",";
-                                                }
+                                boolean courseInList = false;
+                                while (i < prereqs.length && !courseInList) {
+                                    if (prereqs[i].equals(courseCode)) {
+                                        courseInList = true;
+                                        String newPrereqs = "";
+                                        for (int j = 0; j < prereqs.length; j++) {
+                                            if (j != i) {
+                                                newPrereqs += prereqs[j] + ",";
                                             }
-                                            int upperBound = 0;
-                                            if (!newPrereqs.equals("")) {
-                                                upperBound = newPrereqs.length()-1;
-                                            }
-                                            database.child("admin_courses").child(ds.getKey()).child("prerequisites").setValue(newPrereqs.substring(0, upperBound));
                                         }
-                                        i++;
-                                    }
+                                        int upperBound = 0;
+                                        if (!newPrereqs.equals("")) {
+                                            upperBound = newPrereqs.length()-1;
+                                        }
 
+                                        database.child("admin_courses").child(ds.getKey()).child("prerequisites").setValue(newPrereqs.substring(0, upperBound));
+                                    }
+                                    i++;
+                                }
                             }
-                            database.child("admin_courses").child(courseCode).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            database.child("admin_courses").child(courseCode).removeValue()
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
                                         public void onSuccess(Void aVoid) {
                                             Toast.makeText(EditCourses.this, "Course Deleted Successfully", Toast.LENGTH_LONG).show();
@@ -170,11 +144,6 @@ public class EditCourses extends AppCompatActivity {
                     }
                 });
             }
-
-
         });
-
-
-
     }
 }
