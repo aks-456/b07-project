5c676499aa55aa8ba50a33b3dfde06d6b4e20e84
aks-456 <akshath.sohal@gmail.com>
2022-12-05 16:25:39 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/aks-456/b07-project""
    This reverts commit 4cefd5f17660fe0919899d424aef034b00ad9359.

--- B07Project/app/src/main/java/com/example/b07project/DisplayTakenCourseActivity.java
+++ B07Project/app/src/main/java/com/example/b07project/DisplayTakenCourseActivity.java
@@ -8,33 +8,35 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
+
 import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
-public class display_taken_course extends AppCompatActivity implements View.OnClickListener {
+
+public class DisplayTakenCourseActivity extends AppCompatActivity {
 
     ListView listview;
     FirebaseDatabase database;
     DatabaseReference ref;
     List<Course> list;
     Course course;
+    Button btnAdd;
+    Button btnBac;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +47,19 @@ public class display_taken_course extends AppCompatActivity implements View.OnCl
 
         listview = (ListView) findViewById(R.id.takencrslist_view);
         database = FirebaseDatabase.getInstance();
+        FirebaseUser ud = FirebaseAuth.getInstance().getCurrentUser();
+        String userid = ud.getUid();
 
-        String userid = "101";
+
+
         ref = database.getReference("students").child(userid).child("takenCourse");
         list = new ArrayList<>();
-        crsAdapter adapter = new crsAdapter(display_taken_course.this,R.layout.crs_list,list);
+        crsAdapter adapter = new crsAdapter(DisplayTakenCourseActivity.this, R.layout.crs_list, list);
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 list.clear();
-                for(DataSnapshot ds: snapshot.getChildren()){
+                for (DataSnapshot ds : snapshot.getChildren()) {
                     course = ds.getValue(Course.class);
                     list.add(course);
 
@@ -71,13 +76,49 @@ public class display_taken_course extends AppCompatActivity implements View.OnCl
         });
 
 
-    }
+        btnAdd = (Button) findViewById(R.id.tcbuttonAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
 
-    @Override
-    public void onClick(View view) {
-        Toast.makeText(display_taken_course.this, "Theremanually", Toast.LENGTH_LONG).show();
+
+            @Override
+            public void onClick(View view) {
+
+                switchActivities();
+
+            }
+
+
+            public void switchActivities() {
+                Intent switchActivityIntent = new Intent(DisplayTakenCourseActivity.this, AddtakencrsActivity.class);
+                startActivity(switchActivityIntent);
+            }
+        });
+
+        btnBac = (Button) findViewById(R.id.tcbuttonBack);
+        btnBac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivitiestodash();
+
+            }
+
+            //TODO: change activity for switching under -> student home page;
+
+            public void switchActivitiestodash() {
+                Intent switchActivityIntent = new Intent(DisplayTakenCourseActivity.this, AddtakencrsActivity.class);
+                startActivity(switchActivityIntent);
+            }
+        });
+
+
+
+
     }
 }
+
+
+
+
 
 
 
---
+++ B07Project/app/src/main/java/com/example/b07project/LoginPresenter.java
@@ -0,0 +1,28 @@
+package com.example.b07project;
+
+public class LoginPresenter implements LoginContract.LoginListener{
+
+    private LoginContract.LoginView login_view;
+    private LoginInt login_interactor;
+
+    public LoginPresenter(LoginContract.LoginView login_view)
+    {
+        this.login_view = login_view;
+        login_interactor = new LoginInt(this);
+    }
+    public void begin(LoginInformation info)
+    {
+        login_interactor.login(info);
+    }
+
+    @Override
+    public void onSuccess(LoginInformation info) {
+        login_view.onSuccess(info);
+    }
+
+    @Override
+    public void onFailed(String message) {
+        login_view.onFailed(message);
+    }
+
+}
