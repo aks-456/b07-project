236448b3147e6fdbac5197455dfa2ec52fab4e5b
angi963 <angelchiang123@gmail.com>
2022-12-05 22:22:27 -0500

    -unit testing..

--- B07Project/app/src/test/java/com/example/b07project/ExampleUnitTest.java
+++ B07Project/app/src/test/java/com/example/b07project/ExampleUnitTest.java
@@ -2,10 +2,16 @@ package com.example.b07project;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.errorprone.annotations.DoNotMock;
@@ -19,17 +25,60 @@ import com.google.errorprone.annotations.DoNotMock;
 @RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
     @Mock
-    login_activity view;
+    LoginContract.LoginView loginView; //view
 
     @Mock
-    LoginContract.LoginView contract;
+    LoginInt loginint; //model
+
+    @Mock
+    LoginInformation logininformation;
+
 
     @Test
-    public void testPresenter(){
-   //     when(view.->.some method call).thenReturn("abc")
-        //when this method is invoke return abc
-        //when()
+    public void testLoginSuccessful(){
+        String userName = "test@gmail.com";
+        String password = "password";
+        logininformation = new LoginInformation(userName,password);
+        LoginPresenter presenter = new LoginPresenter(loginView);
+        presenter.begin(logininformation);
+        verify(loginView,times(1)).onSuccess(any());
+
     }
+
+    @Test
+    public void testLoginFailed1(){
+        String userName = "test@gmail.com";
+        String password = "12345";
+        logininformation = new LoginInformation(userName,password);
+        LoginPresenter presenter = new LoginPresenter(loginView);
+        presenter.begin(logininformation);
+        verify(loginView,times(1)).onFailed(any());
+
+    }
+
+    @Test
+    public void testLoginFailed2(){
+        String userName = "testgmail.com";
+        String password = "12345";
+        logininformation = new LoginInformation(userName,password);
+        LoginPresenter presenter = new LoginPresenter(loginView);
+        presenter.begin(logininformation);
+        verify(loginView,times(1)).onFailed(any());
+
+    }
+
+    @Test
+    public void testLoginFailed3(){
+        String userName = "testgmail.com";
+        String password = "1234567";
+        logininformation = new LoginInformation(userName,password);
+        LoginPresenter presenter = new LoginPresenter(loginView);
+        presenter.begin(logininformation);
+        verify(loginView,times(1)).onFailed(any());
+
+    }
+
+
 
 
 
